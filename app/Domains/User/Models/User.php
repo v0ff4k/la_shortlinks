f92b70aa18b0d4e9d7c68cf85556b4142cddc84b
worker <10612098+v0ff4k@users.noreply.github.com>
2026-07-02 22:08:22 +0600

    chore: add notice cause implements MustVerifyEmail but not realised, in example

--- app/Domains/User/Models/User.php
+++ app/Domains/User/Models/User.php
@@ -8,6 +8,16 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Domains\Url\Models\Url;
 
+/**
+ * Class User
+ * @todo MustVerifyEmail is declared but there is no mail configuration, no verification routes beyond
+ *     Auth::routes(['verify' => true]), and no queue worker to send emails.
+ *     This will silently block all API access for unverified users.
+ * Проблема остаётся частично. Для полноценной верификации нужно настроить Mail/Queue. Пока оставим интерфейс,
+ *     но важно настроить очередь и отправку писем в прод-е.
+ *
+ * @package App\Domains\User\Models
+ */
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, Notifiable;
