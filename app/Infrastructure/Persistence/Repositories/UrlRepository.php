e41b1b51b8608e5e16bb88df660fff959f532311
worker <10612098+v0ff4k@users.noreply.github.com>
2026-07-02 21:36:59 +0600

    fix: improving hex  architec, interface belongs to the Domain

--- app/Infrastructure/Persistence/Repositories/UrlRepository.php
+++ app/Infrastructure/Persistence/Repositories/UrlRepository.php
@@ -3,12 +3,13 @@
 namespace App\Infrastructure\Persistence\Repositories;
 
 use App\Domains\Url\Models\Url;
+use App\Domains\Url\Repositories\UrlRepositoryInterface;
 
 class UrlRepository implements UrlRepositoryInterface
 {
     public function findByShortCode(string $code): ?Url
     {
-        return Url::where('short_code', $code)->first();
+        return Url::where('short_code', $code)->orWhere('custom_alias', $code)->first();
     }
 
     public function create(array $data): Url
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use App\Infrastructure\Persistence\Repositories\UrlRepository;
 use App\Domains\Url\Repositories\UrlRepositoryInterface;
+use App\Infrastructure\Persistence\Repositories\UrlRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
