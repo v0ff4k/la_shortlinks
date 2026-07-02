a194548a3d97ff1ca97e22ad8bb24d63c92e9737
worker <10612098+v0ff4k@users.noreply.github.com>
2026-07-02 22:03:39 +0600

    chore: RedirectController route is commented out in api.php and moved to web.php, so not used. was for test

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,9 @@
 <?php
 
 use App\Http\Controllers\UrlController;
-//use App\Http\Controllers\RedirectController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'throttle:60,1'])
     ->group(function () { // 60 запросов в минуту
         Route::post('/urls', UrlController::class);
     });
-
-// Route::get('/{code}', [RedirectController::class, '__invoke']);
